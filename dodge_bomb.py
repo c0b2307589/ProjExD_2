18ab09d25685cc10108dacc3e9d714171e64130e
c0b2307589 <c0b2307589@edu.teu.ac.jp>
2024-11-26 14:50:24 +0900

    問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,6 +15,18 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.rect) -> tuple[bool, bool]:
+    "引数で与えられたrectが画面の中か外かを判定する"
+    "引数:こうかとんRect or 爆弾Rect"
+    "戻り値:真理値タプル（横、縦）/画面内:True、画面外:False"
+    yoko,tate = True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    
+    if rct.top  < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -41,6 +53,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -51,8 +67,15 @@ def main():
                 sum_mv[1] -= tpl[1]
         
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
 
         pg.display.update()
